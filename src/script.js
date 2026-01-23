5386358021b7c0a1a184cda2b2668f07f5899edd
Eric Miulli <40775028+EMiulli@users.noreply.github.com>
2026-01-23 15:30:55 -0600

    JS issues

--- src/script.js
+++ src/script.js
@@ -1,13 +1,7 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(anchor.getAttribute('href'))
-      .scrollIntoView({ behavior: 'smooth' });
-  });
-});
-
 const toggle = document.getElementById("theme-toggle");
 
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+
